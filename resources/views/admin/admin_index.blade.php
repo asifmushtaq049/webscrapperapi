9f5e32e02708744b639fe9994065a27d927b3663
Noor Rehman <14051598-027@uog.edu.pk>
2018-07-04 09:14:03 -0700

    error remove

--- resources/views/admin/admin_index.blade.php
+++ resources/views/admin/admin_index.blade.php
@@ -59,36 +59,7 @@
     </div>
 
   </div>
-  <!-- Area Chart Example-->
-  <!-- <style type="text/css">
-    img {
-            height: 650px;
-            background-position: center;
-            background-size: cover;
-           }
-  </style>
-   <div id="banner">
-<div class="inline-block">
-    <img src ="{{URL::asset('../img/one.jpg')}}">
-</div>
-
-<div class="inline-block">
-    <img src ="{{URL::asset('../img/two.png')}}">
-</div>
-
-<div class="inline-block">
-    <img src ="{{URL::asset('../img/three.png')}}">
-</div>
-</div> -->
-  <!-- <div class="card mb-3">
-    <div class="card-header">
-      <img src="{{URL::asset('../img/one.jpg')}}">
-      <img src="{{URL::asset('../img/two.png')}}">
-      <img src="{{URL::asset('../img/three.png')}}">
-      <!-- <i class="fa fa-area-chart"></i> Area Chart Example</div>
-    <div class="card-body">
-      <canvas id="myAreaChart" width="100%" height="30"></canvas>
-    </div> -->
+  
 
   </div> -->
 
