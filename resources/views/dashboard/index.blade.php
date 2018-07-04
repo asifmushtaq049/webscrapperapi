4f0bb739f7a68518b9dc540f7c7d47ba692f4f40
Noor Rehman <14051598-027@uog.edu.pk>
2018-07-04 07:48:38 -0700

    Comment error fixed

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -80,17 +80,6 @@
         <img src ="{{URL::asset('../img/three.png')}}">
     </div>
 </div>
-      <!-- <div class="card mb-3">
-        <div class="card-header">
-          <img src="{{URL::asset('../img/one.jpg')}}">
-          <img src="{{URL::asset('../img/two.png')}}">
-          <img src="{{URL::asset('../img/three.png')}}">
-          <!-- <i class="fa fa-area-chart"></i> Area Chart Example</div>
-        <div class="card-body">
-          <canvas id="myAreaChart" width="100%" height="30"></canvas>
-        </div> -->
-
-      </div> -->
 
     </div>
     @include("dashboard.footer")
